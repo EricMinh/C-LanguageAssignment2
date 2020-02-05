d19814aedadd0bbc5c00fc347e4b687deb4b6617
EricMinh <32127944+EricMinh@users.noreply.github.com>
2020-02-05 04:27:23 -0800

    Update ex24.cpp

--- ex24.cpp
+++ ex24.cpp
@@ -1,3 +1,10 @@
+//Author: Eric Minh Hanh Nguyen
+//This program checks syntax by matching sure all parentheses, square brackets, and braces match...
+//...while ignoring comments, quotes/double quotes, and escape sequences.
+//This program requires the fileOpCl.h to be present and was tested valid on Visual Studio 2017
+//Command Prompt Format: program.exe inputfile.txt outputfile.txt (optional int value)
+//DISCLAIMER: Should any problem in testing ensue, author can provide testing on personal computer
+
 #include "fileOpCl.h"
 
 int main(int argc, const char * argv[]) {
@@ -79,11 +86,13 @@ int main(int argc, const char * argv[]) {
 		}
 	}
 
-	printf("%d, %d, %d, %d, %d, %d\n", lp, rp, ls, rs, lb, rb);
+	printf("\n%d left parentheses, %d right parentheses\n", lp, rp);
+	printf("%dleft square brackets, %d right square brackets\n", ls, rs);
+	printf("%d left braces, %d right braces\n", lb, rb);
 
 	if (lp == rp && ls == rs && lb == rb)
 	{
-		printf("Valid Syntax\n");
+		printf("\nValid Syntax\n");
 	}
 
 	closefiles(fin, fout);
